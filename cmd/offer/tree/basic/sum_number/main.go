a65c6fdaff3c14b67eff56e943dd7f2b9532eaae
lichenyang <lichenyanggloria@sina.com>
2018-11-09 11:04:47 +0800

    add tree1

--- cmd/offer/tree/basic/sum_number/main.go
+++ cmd/offer/tree/basic/sum_number/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"lianshou/cmd/offer/common"
+	"fmt"
 )
 
 func count(root *common.BinaryTreeNode) int {
-	if root == nil{
+	if root == nil {
 		return 0
 	}
 
-	return count(root.Left)
+	leftCount := count(root.Left)
+	rightCount := count(root.Right)
+	if leftCount > rightCount {
+		return leftCount + 1
+	} else {
+		return rightCount + 1
+	}
+}
+
+func main() {
+	//root := &common.BinaryTreeNode{1, &common.BinaryTreeNode{2, nil, nil}, nil}
+	//root := &common.BinaryTreeNode{1, nil, nil}
+	cnt := count(nil)
+	fmt.Println(cnt)
 }
